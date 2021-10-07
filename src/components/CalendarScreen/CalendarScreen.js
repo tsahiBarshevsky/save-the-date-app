803233163362696b8b2f8d3a242eaf91891d3d03
tsahiBarshevsky <tsahi.13@gmail.com>
2021-10-07 17:45:37 +0300

    Dot at maker with events

--- src/components/CalendarScreen/CalendarScreen.js
+++ src/components/CalendarScreen/CalendarScreen.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import Moment from 'moment';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -8,31 +8,50 @@ import { ScrollView } from 'react-native-gesture-handler';
 const CalendarScreen = () => {
 
     const [selectedDate, setSelectedDate] = useState(Moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }));
+    const [marker, setMarker] = useState({ selected: true, marked: false, selectedColor: '#0D5C46' })
     const formatedSelectedDate = selectedDate.format('YYYY-MM-DD').toString();
     const medicines = useSelector(state => state.medicines);
 
-    const filterByDate = (item) => {
-        return (
-            Moment(item.openDate).isSame(selectedDate) ||
-            Moment(item.endDate).isSame(selectedDate)
-        );
+    function containsDate(date) {
+        return function (item) {
+            return (
+                Moment(item.openDate).isSame(date) ||
+                Moment(item.endDate).isSame(date)
+            );
+        }
     }
+
+    const changeMarker = (day) => {
+        const converted = Moment(day.timestamp).set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+        setSelectedDate(converted);
+        if (medicines.filter(containsDate(converted)).length > 0)
+            setMarker({ selected: true, marked: true, selectedColor: 'green' });
+        else
+            setMarker({ selected: true, marked: false, selectedColor: 'green' });
+    }
+
+    useEffect(() => {
+        if (medicines.filter(containsDate(Moment().set({ hour: 0, minute: 0, second: 0, millisecond: 0 }))).length > 0)
+            setMarker({ selected: true, marked: true, selectedColor: 'green' });
+        else
+            setMarker({ selected: true, marked: false, selectedColor: 'green' });
+    }, []);
 
     return (
         <View>
             <Calendar
                 enableSwipeMonths
                 hideArrows
-                onDayPress={(day) => setSelectedDate(Moment(day.timestamp).set({ hour: 0, minute: 0, second: 0, millisecond: 0 }))}
+                onDayPress={(day) => changeMarker(day)}
                 markingType='dot'
                 markedDates={
-                    { [formatedSelectedDate]: { selected: true, marked: true, selectedColor: 'green' } }
+                    { [formatedSelectedDate]: marker }
                 }
             />
             <Text>{selectedDate.format('DD/MM/YYYY HH:mm')}</Text>
             <ScrollView>
                 {
-                    medicines.filter(filterByDate).map((medicine) => {
+                    medicines.filter(containsDate(selectedDate)).map((medicine) => {
                         return (
                             <View key={medicine._id}>
                                 <Text>{medicine.name}</Text>
